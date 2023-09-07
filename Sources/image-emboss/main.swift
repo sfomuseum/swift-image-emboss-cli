b738c4c494e5fe435d0c291b6bc3e003961ad324
sfomuseumbot <sfomuseumbot@localhost>
2023-09-07 09:55:25 -0700

    snapshot: start writing files

--- Sources/image-emboss/main.swift
+++ Sources/image-emboss/main.swift
@@ -5,6 +5,7 @@ import ImageEmboss
 
 public enum Errors: Error {
     case notFound
+    case invalidPath
     case invalidImage
     case cgImage
     case processError
@@ -13,9 +14,12 @@ public enum Errors: Error {
 
 @available(macOS 14.0, *)
 struct ImageEmbossCLI: ParsableCommand {
-
-    @Argument(help:"The path to an image file to extract image subjects from  ")
+    
+    @Option(help:"The path to an image file to extract image subjects from")
     var inputFile: String
+    
+    @Option(help:"Combined...")
+    var combined: Bool
     
     func run() throws {
         
@@ -25,6 +29,10 @@ struct ImageEmbossCLI: ParsableCommand {
             throw(Errors.notFound)
         }
         
+        let source_url = URL(fileURLWithPath: inputFile)
+        let source_root = source_url.deletingLastPathComponent()
+        let source_ext = source_url.pathExtension
+        
         guard let im = NSImage(byReferencingFile:inputFile) else {
             throw(Errors.invalidImage)
         }
@@ -32,18 +40,53 @@ struct ImageEmbossCLI: ParsableCommand {
         guard let cgImage = im.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
             throw(Errors.cgImage)
         }
-        
+                
         let ic = ImageEmboss()
-        let rsp = ic.ProcessImage(image: cgImage, combined: true)
+        let rsp = ic.ProcessImage(image: cgImage, combined: combined)
         
         switch rsp {
         case .failure(let error):
             throw(error)
         case .success(let data):
+            
+            print("WHAT \(data.count)")
+            
+            var i = 1
+            
+            for im in data {
+                
+                let fname = source_url.deletingPathExtension().lastPathComponent
+                let im_fname =  fname + "-" + String(format:"%03d", i) + "." + source_ext
+                
+                let im_url = URL(fileURLWithPath: source_root.absoluteString)
+                let im_path = im_url.appending(path: im_fname)
+                
+                print("SPORK \(im_path)")
 
-            print(data)
+                let im_rsp = im.pngWrite(to: im_path)
+                print("WOO \(im_rsp)")
+                
+                i += 1
+            }
         }
         
+    }
+    
+}
+
+extension NSImage {
+    var pngData: Data? {
+        guard let tiffRepresentation = tiffRepresentation, let bitmapImage = NSBitmapImageRep(data: tiffRepresentation) else { return nil }
+        return bitmapImage.representation(using: .png, properties: [:])
+    }
+    func pngWrite(to url: URL, options: Data.WritingOptions = .atomic) -> Bool {
+        do {
+            try pngData?.write(to: url, options: options)
+            return true
+        } catch {
+            print(error)
+            return false
+        }
     }
 }
 
